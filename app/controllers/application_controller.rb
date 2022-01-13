1dcdf691099a0038d89a22d2b542c47d7f1c937b
EC2 Default User <ec2-user@ip-172-31-81-41.ec2.internal>
2022-01-13 02:05:51 +0000

    [finish]4-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  #deviseの機能が呼び出される直前に『configure_parmitted_parameters』メソッドを実行
+  before_action:configure_parmitted_parameters, if: :devise_controller?
+
+  protected #ストロングパラメータ　private：同じコントローラ内 protected：他のコントローラからの呼び出しもOK
+
+  #受取りを許可するパラメータの変更　nameを受け取れるようにする
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
