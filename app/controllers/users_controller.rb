bf83ef226b1825855a746dc64442c98e11f1977c
EC2 Default User <ec2-user@ip-172-31-81-41.ec2.internal>
2022-01-14 02:51:15 +0000

    [中断]4-15 エラー対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
-    @user = Users.find(params[:id])
-    
+    @user = User.find(params[:id])
+
+    #@post_images というインスタンス変数に、↑で取得したユーザーに紐づく投稿をすべて代入
+    #@user.post_images ⇒ @userに紐づくpost_imagesモデル（テーブル？）のすべてを代入
+    #アソシエーションが通ってるモデル同士で使える記述。
+    @post_images = @user.post_images
   end
 
   def edit
