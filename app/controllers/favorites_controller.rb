3fa5bd27fab0a622e80819eb20ef4cd12e43f17a
ShotaKinoshita <onque.unchain@gmail.com>
2022-01-17 07:59:02 +0000

    [finish]4-20 部分テンプレート導入

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,8 +3,7 @@ class FavoritesController < ApplicationController
   def create
     # URLから対象の投稿のレコードを検索、post_image変数へ代入
     post_image = PostImage.find(params[:post_image_id])
-    # 全然わからん…
-    # 後半 Favoriteモデルのテーブルに新規レコード枠をnew、post_image_idカラムは今のpost_iamgeのidカラムを指定)
+    # current_userに結びつく(user_idがcurrent_userのidとなる)favoritesを生成、post_image_idカラムは今のpost_iamgeのidカラムを指定)
     favorite = current_user.favorites.new(post_image_id:post_image.id)
     favorite.save
     redirect_to post_image_path(post_image)
@@ -12,6 +11,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     post_image = PostImage.find(params[:post_image_id])
+    # current_userに結びつく(user_idがcurrent_userのidと同じ)favoritesテーブルからpost_image_idが現在のpost_imageのidと同じものを探してdestroy
     favorite = current_user.favorites.find_by(post_image_id:post_image.id)
     favorite.destroy
     redirect_to post_image_path(post_image)
