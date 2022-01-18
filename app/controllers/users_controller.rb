1a4e420b75568cc69a4d5e5fb60c55c112eb00df
ShotaKinoshita <onque.unchain@gmail.com>
2022-01-18 00:10:29 +0000

    [finish]4-22 ページング機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     #@post_images というインスタンス変数に、↑で取得したユーザーに紐づく投稿をすべて代入
     #@user.post_images ⇒ @userに紐づくpost_imagesモデル（テーブル？）のすべてを代入
     #アソシエーションが通ってるモデル同士で使える記述。
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
